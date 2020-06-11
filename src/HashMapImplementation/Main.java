819eade957818c79d22b31628b105f44c3225636
Verlierer-Versager <62723185+Verlierer-Versager@users.noreply.github.com>
2020-06-12 01:03:20 +0300

    Update Main.java

--- src/HashMapImplementation/Main.java
+++ src/HashMapImplementation/Main.java
@@ -10,12 +10,14 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scn = new Scanner(System.in);
         String fileName = scn.nextLine();
+
         File file = new File(fileName);
         Scanner in = new Scanner(new File(fileName));
         String text = in.nextLine();
-        //C:\Users\sestr\IdeaProjects\Task6aisd\src\Text.txt
+
         Task task = new Task();
         ArrayList<Occurrence> list = task.solution(text);
+
         for (Occurrence elem : list) {
             System.out.println(elem.getChars() + " = " + elem.getOccurrence());
         }
